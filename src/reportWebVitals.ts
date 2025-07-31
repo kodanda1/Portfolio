beeb5ac9017b84bab0e94005f254303db9442791
kodanda1 <kodandapuramvaruntej@gmail.com>
2025-07-31 12:57:25 -0400

    Update GitHub Pages deployment with latest build

--- src/reportWebVitals.ts
+++
@@ -1,8 +0,0 @@
-const reportWebVitals = (onPerfEntry?: any) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    // Web vitals reporting disabled for now
-    console.log('Web vitals reporting disabled');
-  }
-};
-
-export default reportWebVitals;
